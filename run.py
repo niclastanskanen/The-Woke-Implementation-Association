81480723ed368d2ad10212dcddf11320677c7ab1
Tinks18 <102434085+Tinks18@users.noreply.github.com>
2023-02-17 10:24:39 +0000

    Tina (#26)
    * add basic nav elements
    
    * setting up base.html and routing
    
    * add nav-bar
    
    * add bootstrap nav bar with branding
    
    * adjust bootstrap to version4 for a working navbar burger menu
    
    * add basic flask framework
    
    ---------
    
    Co-authored-by: Niclas <niclastanskanen@hotmail.com>

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
