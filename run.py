445a7d7a18f069110817d494566f3d09f625719e
hypergeek-dev <32468376+hypergeek-dev@users.noreply.github.com>
2023-02-19 09:34:58 +0100

    Dennis (#48)
    * Content for map
    
    * Donation page
    
    * content for landing page
    
    * Syntax correction
    
    * Add procfile
    
    * json for heroku and automated testing
    
    * env.py
    
    * User-stories and colorscheme
    
    * Static src in run.py
    
    * Readme changes and adding image to index
    
    * Changing folder to static root
    
    * Added image to index
    
    * Addint background div to index
    
    * Removing redundant empty files

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
@@ -34,3 +35,6 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
+
+
